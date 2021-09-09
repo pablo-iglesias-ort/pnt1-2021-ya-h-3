964b7ee37927079511c9365db5ba0e6ec140543d
matisebas1989 <matiassgs@gmail.com>
2021-09-09 19:39:29 -0300

    Agrego atributos a Turno y creo las clases Paciente y Profesional que heredan de Persona

--- AgendaTurnos/Models/Turno.cs
+++ AgendaTurnos/Models/Turno.cs
@@ -2,12 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace AgendaTurnos.Models
 {
     public class Turno
     {
+
+        [Required]
         public Guid Id { get; set; }
+
+        [Required]
+        public DateTime Fecha { get; set; }
+
+        [Required]
+        public Boolean Confirmado { get; set; }
+
+        [Required]
+        public Boolean Activo { get; set; }
+
+        [Required]
+        public DateTime FechaSolicitud { get; set; }
+
+        [Required]
+        public Paciente Paciente { get; set; }
+
+        [Required]
+        public Profesional Profesional { get; set; }
+
+        public String DescripcionCancelacion { get; set; }
+
     }
 
 }
