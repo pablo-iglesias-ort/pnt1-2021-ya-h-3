ddb8ee92a825011ac2a16cd88ee9ae6d1fbaac8c
matisebas1989 <matiassgs@gmail.com>
2021-09-09 22:28:37 -0300

    agrego los diagramas, y empiezo a hacer cambios ya que al leer el git, cambio el diagrama que teniamos.

--- AgendaTurnos/Models/Usuario.cs
+++ AgendaTurnos/Models/Usuario.cs
@@ -11,22 +11,21 @@ namespace AgendaTurnos.Models
         [Required]
         public Guid Id { get; set; }
 
-
         [Required]
         public string  Nombre { get; set; }
-
 
         [Required]
         [EmailAddress(ErrorMessage ="Por favor, ingresar un email valido.")]
         public string Email { get; set; }
 
-
         [Required]
         public DateTime FechaAlta { get; set; }
-
 
         [Required]
         [MinLength(8, ErrorMessage = "La contraseña debe tener como minimo 8(ocho) caracteres.")]
         public string Password { get; set; }
+
+        [Required]
+        public String rol { get; set; }
     }
 }
