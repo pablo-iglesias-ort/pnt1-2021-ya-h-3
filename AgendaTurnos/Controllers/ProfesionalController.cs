91225ec463cb1690148b315741b059427ca9e7e1
Matías Pintow <gow.mt@hotmail.com>
2021-09-14 03:07:10 -0300

    Completo el controlador de profesional.

--- AgendaTurnos/Controllers/ProfesionalController.cs
+++ AgendaTurnos/Controllers/ProfesionalController.cs
@@ -14,6 +14,42 @@ namespace AgendaTurnos.Controllers
     {
         private readonly AgendaTurnosContext _context;
 
+        static List<Profesional> profesionales = new List<Profesional>()
+        {
+            new Profesional()
+            {
+                Id = Guid.NewGuid(),
+                Nombre = "Joel",
+                Apellido = "Salum",
+                Email = "kskplusst@hotmail.com",
+                FechaAlta = new DateTime(2021, 02, 07),
+                Password = "45345",
+                Telefono = "2222222",
+                Direccion = "falsa 123",
+                Dni = "235789",
+                Rol = "Profesional",
+                Matricula = "123456789",
+                HoraInicio = new DateTime(2021, 02, 07, 08, 00, 00),
+                Turnos = new List<Turno> (),
+            },
+                        new Profesional()
+            {
+                Id = Guid.NewGuid(),
+                Nombre = "Matias",
+                Apellido = "Pintow",
+                Email = "gow.mt@hotmail.com",
+                FechaAlta = new DateTime(2021, 01, 07),
+                Password = "987654321",
+                Telefono = "2222222",
+                Direccion = "verdadero 123",
+                Dni = "1325789",
+                Rol = "Profesional",
+                Matricula = "987643211",
+                HoraInicio = new DateTime(2021, 02, 07, 08, 30, 00),
+                Turnos = new List<Turno> (),
+            }
+        };
+
         public ProfesionalController(AgendaTurnosContext context)
         {
             _context = context;
@@ -22,7 +58,8 @@ namespace AgendaTurnos.Controllers
         // GET: Profesional
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Profesional.ToListAsync());
+            return View(profesionales);
+        //    return View(await _context.Profesional.ToListAsync());
         }
 
         // GET: Profesional/Details/5
@@ -33,8 +70,9 @@ namespace AgendaTurnos.Controllers
                 return NotFound();
             }
 
-            var profesional = await _context.Profesional
-                .FirstOrDefaultAsync(m => m.Id == id);
+            //var profesional = await _context.Profesional
+            //  .FirstOrDefaultAsync(m => m.Id == id);
+            var profesional = profesionales.FirstOrDefault(m => m.Id == id);
             if (profesional == null)
             {
                 return NotFound();
@@ -59,8 +97,9 @@ namespace AgendaTurnos.Controllers
             if (ModelState.IsValid)
             {
                 profesional.Id = Guid.NewGuid();
-                _context.Add(profesional);
-                await _context.SaveChangesAsync();
+                //_context.Add(profesional);
+                //await _context.SaveChangesAsync();
+                profesionales.Add(profesional);
                 return RedirectToAction(nameof(Index));
             }
             return View(profesional);
@@ -74,7 +113,9 @@ namespace AgendaTurnos.Controllers
                 return NotFound();
             }
 
-            var profesional = await _context.Profesional.FindAsync(id);
+            //var profesional = await _context.Profesional.FindAsync(id);
+            var profesional = profesionales.FirstOrDefault(m => m.Id == id);
+
             if (profesional == null)
             {
                 return NotFound();
@@ -87,7 +128,7 @@ namespace AgendaTurnos.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, [Bind("Matricula,HoraInicio,HoraFin,Id,Nombre,Apellido,Email,FechaAlta,Password,Telefono,Direccion,Dni,Rol")] Profesional profesional)
+        public async Task<IActionResult> Edit(Guid id, [Bind("Matricula,HoraInicio,HoraFin,Id,Nombre,Apellido,Email,FechaAlta,Password,Telefono,Direccion,Dni")] Profesional profesional)
         {
             if (id != profesional.Id)
             {
@@ -98,8 +139,22 @@ namespace AgendaTurnos.Controllers
             {
                 try
                 {
-                    _context.Update(profesional);
-                    await _context.SaveChangesAsync();
+                    //_context.Update(profesional);
+                    //await _context.SaveChangesAsync();
+                    var profesionalExistente = profesionales.FirstOrDefault(m => m.Id == id);
+                    profesionalExistente.Matricula = profesional.Matricula;
+                    profesionalExistente.HoraInicio = profesional.HoraInicio;
+                    profesionalExistente.HoraFin = profesional.HoraFin;
+                    profesionalExistente.Nombre = profesional.Nombre;
+                    profesionalExistente.Apellido = profesional.Apellido;
+                    profesionalExistente.Email = profesional.Email;
+                    profesionalExistente.FechaAlta = profesional.FechaAlta;
+                    profesionalExistente.Password = profesional.Password;
+                    profesionalExistente.Telefono = profesional.Telefono;
+                    profesionalExistente.Direccion = profesional.Direccion;
+                    profesionalExistente.Dni = profesional.Dni;
+
+
                 }
                 catch (DbUpdateConcurrencyException)
                 {
@@ -125,8 +180,11 @@ namespace AgendaTurnos.Controllers
                 return NotFound();
             }
 
-            var profesional = await _context.Profesional
-                .FirstOrDefaultAsync(m => m.Id == id);
+            //var profesional = await _context.Profesional
+
+            //.FirstOrDefaultAsync(m => m.Id == id);
+            var profesional = profesionales.FirstOrDefault(m => m.Id == id);
+
             if (profesional == null)
             {
                 return NotFound();
@@ -140,9 +198,11 @@ namespace AgendaTurnos.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(Guid id)
         {
-            var profesional = await _context.Profesional.FindAsync(id);
-            _context.Profesional.Remove(profesional);
-            await _context.SaveChangesAsync();
+            //var profesional = await _context.Profesional.FindAsync(id);
+            //_context.Profesional.Remove(profesional);
+            //await _context.SaveChangesAsync();
+            var profesional = profesionales.FirstOrDefault(m => m.Id == id);
+            profesionales.Remove(profesional);
             return RedirectToAction(nameof(Index));
         }
 
