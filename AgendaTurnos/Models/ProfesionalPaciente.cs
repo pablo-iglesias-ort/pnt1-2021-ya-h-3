ef75c689e955c4d5dd47b8c6f49e645a2f08756d
Matías Pintow <gow.mt@hotmail.com>
2021-11-02 20:02:15 -0300

    Agrego modelo ProfesionalPaciente guardado

--- AgendaTurnos/Models/ProfesionalPaciente.cs
+++ AgendaTurnos/Models/ProfesionalPaciente.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,20 @@ namespace AgendaTurnos.Models
 {
     public class ProfesionalPaciente
     {
+        [Key]
+        public Guid Id { get; set; }
+
+        //relacion con otras entidades.
+
+        [ForeignKey(nameof(Paciente))]
+
+        public Guid PacienteId { get; set; }
+        public Paciente Paciente { get; set; }
+
+        [ForeignKey(nameof(Profesional))]
+        public Guid Profesionald { get; set; }
+        public Profesional Profesional { get; set; }
+
+
     }
 }
